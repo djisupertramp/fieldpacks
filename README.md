5d41c00c9eab6c26fc17354eb6d5bd28a33f7a47
djisupertramp <49781059+djisupertramp@users.noreply.github.com>
2022-06-09 16:52:33 +0200

    core upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Jeremy Janin Blog
+
+https://jeremyjanin.com
+This is automatically synced with Netlify
+
+# License
+
+The following directories and their contents are Copyright Jeremy Janin.
+Less Theme created by Yannick Schutz (https://github.com/ys) & Grégory Mignard (https://github.com/gmignard/)
+
+You may not reuse anything therein without my permission:
+.content/
+
+All other directories and files are MIT Licensed.
---
+++ themes/less/assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new LazyLoad({
+    elements_selector: ".lazyload",
+    skip_invisible: false
+  });
+  photoswipeSimplify.init({
+    history: false,
+    bgOpacity: 1,
+    zoomEl: false,
+    shareEl: false,
+    counterEl: false,
+    fullscreenEl: false,
+    closeEl: false,
+    captionEl: false,
+    arrowEl: false,
+    barsSize: { top: 44, bottom: 44, left: 44, right: 44 }
+  });
+})
